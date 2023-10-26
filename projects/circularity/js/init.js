ea74304e55a6aa6d722f08ae82c590c5421b4099
Aeshus <87614336+Aeshus@users.noreply.github.com>
2023-10-26 18:02:11 -0500

    circularity done

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -21,12 +21,24 @@ var init = function (window) {
         
         // TODO 1 : Declare and initialize our variables
 
+        var circle;
+        var circles = [];
+
 
         // TODO 2 : Create a function that draws a circle 
-        
+
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
 
+        for (var i = 0; i < 100; i++) {
+            drawCircle();
+        }
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -40,11 +52,18 @@ var init = function (window) {
         function update() {
             // TODO 4 : Update the circle's position //
 
+            // (Deleted for TODO 9)
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+
+            // (Deleted for TODO 9)
 
             // TODO 9 : Iterate over the array
+
+            for (var i = 0; i < circles.length; i++) {
+                physikz.updatePosition(circles[i]);
+                game.checkCirclePosition(circles[i]);
+            }
            
             
         }
@@ -62,8 +81,21 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
 
+            // if it goes off to the left, go to right
+            if ( circle.x < 0 ) {
+                circle.x = canvas.width;
+            }
+            
+            // if it goes off to the bottom, go to top
+            if ( circle.y > canvas.height ) {
+                circle.y = 0;
+            }
+
+            // if it goes off to the top, go to bottom
+            if ( circle.y < 0 ) {
+                circle.y = canvas.height;
+            }
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
