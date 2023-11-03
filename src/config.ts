b6ec96d81201604e76baacacf245b9943b0a9af2
Aeshus <87614336+Aeshus@users.noreply.github.com>
2023-11-02 21:52:13 -0500

    feat: Finish up /writing & index

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,6 @@
-
 /* https://github.com/one-aalam/astro-ink */
 export const SITE = {
-  author: "Aeshus",
-  title: "Aeshus.com",
-  url: "Aeshus.com"
-}
+  author: 'Aeshus',
+  title: 'Aeshus.com',
+  url: 'Aeshus.com'
+};
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,31 +1,31 @@
-import { z, defineCollection } from "astro:content";
+import { z, defineCollection } from 'astro:content';
 
 const writingsCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-      title: z.string(),
-      description: z.string(),
-      date: z.date(),
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }),
-      tags: z.array(z.string())
-    })
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string()
+    }).optional(),
+    tags: z.array(z.string()).optional()
+  })
 });
 
 const projectsCollection = defineCollection({
-    type: 'content',
-    schema: z.object({
-      title: z.string(),
-      description: z.string(),
-      startDate: z.date(),
-      endDate: z.date(),
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }),
-    })
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    startDate: z.date(),
+    endDate: z.date().optional(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string()
+    }).optional()
+  })
 });
 
 export const collections = {
