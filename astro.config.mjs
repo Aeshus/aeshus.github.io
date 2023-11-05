c629327ff19f0509ef26b58ba18a0713cc3efcd1
Aeshus <87614336+Aeshus@users.noreply.github.com>
2023-11-05 13:56:27 -0600

    chore: Add mdx as dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,25 @@
 import { defineConfig } from 'astro/config';
-
-
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-
+import mdx from "@astrojs/mdx";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+
 // https://astro.build/config
 export default defineConfig({
-
   vite: {
     plugins: [],
     resolve: {
       alias: {
         '$': path.resolve(__dirname, './src')
       }
-    },  
-  }
-
+    }
+  },
+  markdown: {
+    syntaxHighlight: "shiki",
+    shikiConfig: { theme: "css-variables" },
+    gfm: true,
+  },
+  integrations: [mdx()]
 });
