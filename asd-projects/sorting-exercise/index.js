f327117d938a6a192b3677ff8413a98d459e1b41
Aeshus <git@aeshus.com>
2024-05-06 18:44:49 -0500

    feat: finish

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -15,14 +15,66 @@ The CSS ids you will work with are:
 
 // TODO 2: Implement bubbleSort
 
+async function bubbleSort(lst) {
+	for (let i = 0; i < lst.length - 1; i++) {
+		for (let j = lst.length - 1; j > i; j--) {
+			if (lst[j].value < lst[j - 1].value){
+				swap(lst, j, j - 1)
+
+				updateCounter(bubbleCounter);
+				await sleep();
+			}
+		}
+	}
+}
 
 // TODO 3: Implement quickSort
+async function quickSort(array, left, right){
+	if ((right - left) > 0) {
+		let index = await partition(array, left, right);
 
+		if (left < (index-1))
+			await quickSort(array, left, index - 1);
+		if (index < right)
+			await quickSort(array, index, right);
+	}
+}
 
 // TODOs 4 & 5: Implement partition
+async function partition(array, left, right) {
+	// Taking the average should not be the pivot.
+	// Rather, it should be the median of the first, middle, and last values...
+	let pivot = array[Math.floor((right + left)/2)].value;
+
+	while (left < right) {
+		
+		while (array[left].value < pivot){
+		left++;
+		}
+
+		while (array[right].value > pivot){
+			right--;
+		}
+
+		if (left < right) {
+			swap(array, left, right);
+			updateCounter(quickCounter);
+		}
+		await sleep();
+	}
+
+	return left + 1;
+}
 
 
 // TODO 1: Implement swap
+function swap(lst, i, j){
+	let tmp = lst[j];
+	lst[j] = lst[i];
+	lst[i] = tmp;
+	console.log("Swapping " + lst[i].value + " and " + lst[j].value);
+	drawSwap(lst, i, j);
+}
 
 
 ///////////////////////////////////////////////////////////////////////
